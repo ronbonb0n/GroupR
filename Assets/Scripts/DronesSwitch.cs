de337589308bf5e262ee4205de138533d4a2346c
Jerry Loi <45759373+astro2049@users.noreply.github.com>
2022-10-25 12:35:03 +0100

    Master Kill Switch for Drones

---
+++ Assets/Scripts/DronesSwitch.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class DronesSwitch : MonoBehaviour
+{
+    private GameObject[] drones;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        drones = GameObject.FindGameObjectsWithTag("Drone");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            foreach (var drone in drones)
+            {
+                drone.GetComponent<DroneController>().isActivated = false;
+            }
+        }
+    }
+}
