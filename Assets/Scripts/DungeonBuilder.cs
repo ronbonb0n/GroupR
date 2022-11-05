e5611857b75bc5d7f9e2348dc609bc23324e486c
James Joslin <74412992+James-Joslin@users.noreply.github.com>
2022-11-05 12:44:52 +0000

    Dungeon assets and lookup within builder script

--- Assets/Scripts/DungeonBuilder.cs
+++ Assets/Scripts/DungeonBuilder.cs
@@ -7,63 +7,119 @@ using UnityEditor.AI;
 [ExecuteInEditMode]
 public class DungeonBuilder : MonoBehaviour
 {
+    [Header("Consistent tile width and length")] // Currently only supports square blocks
     public int blockSize = 6;
+
+    [Header("Dungeon Bitmap")]
+    public Texture2D dungeonTexture;
+
+    [Header("Open Floor Prefab")]
+    public  GameObject openArea;
+
+    [Header("Outer Corner Prefabs")]
+    public  GameObject topRightOuterCorner;
+    public  GameObject topLeftOuterCorner;
+    public  GameObject bottomRightOuterCorner;
+    public  GameObject bottomLeftOuterCorner;
+
+    [Header("Inner Corner Prefabs")]
+    public  GameObject topRightInnerCorner;
+    public  GameObject topLeftInnerCorner;
+    public  GameObject bottomRightInnerCorner;
+    public  GameObject bottomLeftInnerCorner;
+
+    [Header("Wall Prefabs")]
+    public  GameObject leftWall;
+    public  GameObject rightWall;
+    public  GameObject topWall;
+    public  GameObject bottomWall;
+
+    [Header("Corridor Prefabs")]
+    public GameObject upDownCorridor;
+    public GameObject acrossCorridor;
+
+    [Header("Doorway Prefabs")]
+    public  GameObject leftDoorway;
+    public  GameObject rightDoorway;
+    public  GameObject topDoorway;
+    public  GameObject bottomDoorway;
+
+    [Header("Build level")]
     public bool execute;
-
     // Tuple for RGB data of each pixel
-    (float, float, float) pixelData;
 
+    private GameObject parent;
+    (float, float, float) pixelData;
     // 2D list to contain entire image data
     List<List<(float, float, float)>> dungeonData = new List<List<(float, float, float)>>();
-
-    public Texture2D dungeonTexture;
-    // list<> dungeonList
-    // private GameObject parent;
-
-    // [Header("Wall Sections")]
-    // public  GameObject leftWall;
-    // public  GameObject rightWall;
-    // public  GameObject topWall;
-    // public  GameObject bottomWall;
-
-    // []
-    // public  GameObject Bridge;
-    // public  GameObject TowerCube;
-    // // public  GameObject WaterCube;
-    // public  GameObject SciFiBridge;
-    // Dictionary<float, GameObject> TileDict = new Dictionary<float, GameObject>();
+    Dictionary<(float, float, float), GameObject> dungeonTileDict = new Dictionary<(float, float, float), GameObject>();
     
     private void Update() {
         if (execute)
         {
-            // TileDict.Clear();
-            dungeonData.Clear();
-            sceneSetup();
+            buildLookup();
+            buildDungeonArray();
+            buildScene();
             execute = false;
         }
     }
 
-    public void sceneSetup()
+    public void buildScene()
     {
         
-        // if (parent != null)
-        // {
-        //     DestroyImmediate(parent);
-        // }
-        // parent = new GameObject("Parent");
+        if (parent != null)
+        {
+            DestroyImmediate(parent);
+        }
+        parent = new GameObject("Parent");
 
-        // TileDict.Add(1.0f, GrassCube);
-        // TileDict.Add(0.9f, DeepGrassCube);
-        // TileDict.Add(0.8f, DyingGrassCube);
-        // TileDict.Add(0.7f, DeadCube);
-        // TileDict.Add(0.6f, Bridge);
-        // TileDict.Add(0.5f, TowerCube);
-        // // TileDict.Add(0.0f, WaterCube);
-        // TileDict.Add(0.4f, SciFiBridge);
-        
+        for (int i = 0; i < dungeonData.Count; i++)
+        {
+            Debug.Log(string.Join(", ", dungeonData[i]));
+        }
+
+    }
+
+    public void buildLookup ()
+    {
+        // clear old data
+        dungeonTileDict.Clear();
+
+        // Populate tile lookup
+        // open area
+        dungeonTileDict.Add((1.0f, 1.0f, 1.0f), openArea);
+        // outer corners
+        dungeonTileDict.Add((0.2f, 0.4f, 0.3f), topRightOuterCorner);
+        dungeonTileDict.Add((0.0f, 1.0f, 1.0f), topLeftOuterCorner);
+        dungeonTileDict.Add((0.3f, 1.0f, 0.4f), bottomRightOuterCorner);
+        dungeonTileDict.Add((1.0f, 1.0f, 0.0f), bottomLeftOuterCorner);
+        // inner corners
+        dungeonTileDict.Add((1.0f, 0.7f, 1.0f), topRightInnerCorner);
+        dungeonTileDict.Add((0.8f, 1.0f, 0.5f), topLeftInnerCorner);
+        dungeonTileDict.Add((1.0f, 0.5f, 0.6f), bottomRightInnerCorner);
+        dungeonTileDict.Add((1.0f, 0.7f, 0.0f), bottomLeftInnerCorner);
+        // walls
+        dungeonTileDict.Add((0.0f, 0.0f, 1.0f), leftWall);
+        dungeonTileDict.Add((0.0f, 1.0f, 0.0f), rightWall);
+        dungeonTileDict.Add((0.5f, 0.6f, 1.0f), topWall);
+        dungeonTileDict.Add((0.2f, 0.5f, 0.2f), bottomWall);
+        // corridors
+        dungeonTileDict.Add((0.2f, 0.3f, 0.5f), upDownCorridor);
+        dungeonTileDict.Add((1.0f, 0.6f, 0.3f), acrossCorridor);
+        // doorways
+        dungeonTileDict.Add((0.3f, 0.3f, 0.3f), leftDoorway);
+        dungeonTileDict.Add((0.7f, 0.7f, 0.7f), rightDoorway);
+        dungeonTileDict.Add((1.0f, 0.0f, 0.3f), topDoorway);
+        dungeonTileDict.Add((1.0f, 0.3f, 0.0f), bottomDoorway);
+    }
+
+    public void buildDungeonArray()
+    {
+        dungeonData.Clear();
+
         for (int i = 0; i < dungeonTexture.width; i++)
         {
-            // List for each row of pixels - not sure why it has to be made new and not just cleared
+            // List for each row of pixels - not sure why it has to be made new and not just cleared per iteration
             List<(float, float, float)> pixelRow = new List<(float, float, float)>();
 
             for (int j = 0; j < dungeonTexture.height; j++)
@@ -73,7 +129,6 @@ public class DungeonBuilder : MonoBehaviour
                     Mathf.Round(dungeonTexture.GetPixel(i, j).g * 10f) / 10f,
                     Mathf.Round(dungeonTexture.GetPixel(i, j).b * 10f) / 10f
                 );
-                // Debug.Log(pixelData);
 
                 pixelRow.Add(pixelData);
                 // if (pixel_g != 0.0f)
@@ -95,11 +150,5 @@ public class DungeonBuilder : MonoBehaviour
         }
         // NavMeshBuilder.ClearAllNavMeshes();
         // NavMeshBuilder.BuildNavMesh();
-
-        for (int i = 0; i < dungeonData.Count; i++)
-        {
-            Debug.Log(string.Join(", ", dungeonData[i]));
-        }
-
     }
 }
