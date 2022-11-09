8ae47656a6aa78114c0feab37857bae46b227fa0
Jerry Loi <45759373+astro2049@users.noreply.github.com>
2022-11-09 13:01:25 +0000

    Drone - laser sight

--- Assets/Scripts/AI/LaserSight.cs
+++ Assets/Scripts/AI/LaserSight.cs
@@ -1,66 +1,97 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class LaserSight : MonoBehaviour
 {
     public float fov = 90f;
-    public int sliceCount = 10;
-    public float viewDistance = 10f;
+    public int sliceCount = 20;
+    private float angleIncrease;
+    private float viewDistance;
     
-    public LayerMask obstructionMask;
+    private Vector3[] vertices;
+    private Vector2[] uv;
+    private int[] indices;
+    
+    private Mesh mesh;
+
+    private float laserPitch;
 
     // Start is called before the first frame update
     void Start()
     {
-        Mesh mesh = new Mesh();
-        GetComponent<MeshFilter>().mesh = mesh;
+        FieldOfView fieldOfView = GetComponentInParent<FieldOfView>();
+        viewDistance = fieldOfView.radius;
+        laserPitch = (float) Math.Asin(transform.position.y / fieldOfView.radius);
 
-        float angleIncrease = fov / sliceCount;
-        float angle = 0;
-        Vector3 origin = Vector3.zero;
-        Vector3[] vertices = new Vector3[sliceCount + 2];
-        Vector2[] uv = new Vector2[vertices.Length];
-        int[] indices = new int[sliceCount * 3];
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        angleIncrease = fov / sliceCount;
         
-        vertices[0] = Vector3.zero;
+        vertices = new Vector3[sliceCount + 2];
+        uv = new Vector2[vertices.Length];
+        indices = new int[sliceCount * 3];
+        
+        UpdateLaserSight();
         int vertexIndex = 1;
         int triangleIndex = 0;
         for (int i = 0; i <= sliceCount; i++)
         {
-            Vector3 vertex = origin + RotateVector(angle) * viewDistance;
-            RaycastHit hit;
-            if (Physics.Raycast(origin, RotateVector(angle), out hit, viewDistance))
-            {
-                vertex = hit.point;
-            }
-            vertices[vertexIndex] = vertex;
-            angle += angleIncrease;
-
             if (i >= 1)
             {
                 indices[triangleIndex] = 0;
                 indices[triangleIndex + 1] = vertexIndex - 1;
                 indices[triangleIndex + 2] = vertexIndex;
-
                 triangleIndex += 3;
             }
-
             vertexIndex++;
         }
-
-        mesh.vertices = vertices;
         mesh.uv = uv;
         mesh.triangles = indices;
+
+        StartCoroutine(LaserSightRoutine());
     }
 
     private Vector3 RotateVector(float angle)
     {
         float angleRad = angle * (Mathf.PI / 180f);
-        return new Vector3(Mathf.Cos(angleRad) - Mathf.Sin(angleRad), -0.5f, Mathf.Sin(angleRad) + Mathf.Cos(angleRad));
+        return new Vector3(Mathf.Cos(angleRad) - Mathf.Sin(angleRad), (float) -Math.Sin(laserPitch), Mathf.Sin(angleRad) + Mathf.Cos(angleRad));
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    private IEnumerator LaserSightRoutine()
     {
-        
+        WaitForSeconds wait = new WaitForSeconds(1f);
+
+        while (true)
+        {
+            yield return wait;
+            UpdateLaserSight();
+        }
+    }
+    
+    void UpdateLaserSight()
+    {
+        float angle = 0;
+
+        vertices[0] = Vector3.zero;
+        int vertexIndex = 1;
+        for (int i = 0; i <= sliceCount; i++)
+        {
+            Vector3 vertex = Vector3.Normalize(RotateVector(angle)) * viewDistance;
+            
+            // RaycastHit hit;
+            // Vector3 destination = Quaternion.AngleAxis(transform.eulerAngles.y, Vector3.up) * vertex;
+            // if (Physics.Raycast(transform.position, destination, out hit, viewDistance))
+            // {
+            //     Debug.DrawLine(transform.position, hit.point, Color.white, 0.01f);
+            //     vertex = RotateVector(angle) * hit.distance;
+            // }
+            
+            vertices[vertexIndex] = vertex;
+            angle += angleIncrease;
+            vertexIndex++;
+        }
+
+        mesh.vertices = vertices;
     }
 }
