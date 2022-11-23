53a67b9914fdcd02d82b4fe447d4b0cb6575a510
Jerry Loi <45759373+astro2049@users.noreply.github.com>
2022-11-23 18:07:36 +0000

    Drone Behaviors+

--- Assets/Scripts/AI/LookAroundState.cs
+++ Assets/Scripts/AI/LookAroundState.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class LookAroundState : IDroneState
 {
+    private Vector3 lookAt;
+    
     public IDroneState DoState(DroneController drone)
     {
         if (!drone.isActivated)
@@ -16,6 +18,7 @@ public class LookAroundState : IDroneState
         {
             return drone.patrolState;
         }
+        drone.transform.forward = Vector3.RotateTowards(drone.transform.forward, lookAt, 2 * Time.deltaTime, 0.8f * Time.deltaTime);
         drone.lookAroundCountDownTimer -= Time.deltaTime;
         return drone.lookAroundState;
     }
@@ -26,10 +29,31 @@ public class LookAroundState : IDroneState
         drone.SetStateText("Look Around", Color.green);
         drone.SetLaserColor(Color.green);
         drone.lookAroundCountDownTimer = Random.Range(1.0f, 2.0f);
+        GetLookDirection(drone);
     }
 
     public void onExit(DroneController drone)
     {
         drone.lookAroundCountDownTimer = 0;
+        lookAt = Vector3.forward;
+    }
+
+    private void GetLookDirection(DroneController drone)
+    {
+        float angle = 0;
+        float maxDistance = -1;
+        RaycastHit hit;
+        for (int i = 0; i < 4; i++, angle += 90)
+        {
+            Vector3 direction = Quaternion.AngleAxis(angle, Vector3.up) * drone.transform.forward;
+            Debug.DrawRay(drone.fieldOfView.transform.position, direction * 15, Color.green, 5f);
+            Physics.Raycast(drone.fieldOfView.transform.position, direction * 15, out hit);
+            if (hit.distance > maxDistance)
+            {
+                maxDistance = hit.distance;
+                lookAt = direction;
+            }
+        }
+        Debug.DrawRay(drone.fieldOfView.transform.position, lookAt * 15, Color.magenta, 5f);
     }
 }
