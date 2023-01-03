23aea93268c04be9c728abbb997ae5ca4a528b64
James <74412992+James-Joslin@users.noreply.github.com>
2023-01-03 15:52:44 +0000

    Drone audio
    drone audio 2
    
    Revert "drone audio 2"
    
    This reverts commit b98105b76e3e26f098263ee166c553a1040d1b93.
    
    Revert "Drone audio"
    
    This reverts commit 211462e35c375fc6bcc081eb8d54ba8d0e335129.
    
    Revert "Drone audio"
    
    This reverts commit 211462e35c375fc6bcc081eb8d54ba8d0e335129.
    
    Drone Audio 3D

--- Assets/Scripts/AI/States/DeactivatedState.cs
+++ Assets/Scripts/AI/States/DeactivatedState.cs
@@ -14,14 +14,13 @@ public class DeactivatedState : IDroneState
         drone.SetScannerColor(Color.red);
         drone.navMeshAgent.enabled = false;
         drone.skinnedMesh.AddComponent<Rigidbody>();
-        drone.rotorAudioPlayer.enabled = false;
 
         // Deactivate scanners and text
         for(int i=0; i<drone.body.transform.childCount; i++)
         {
             drone.body.transform.GetChild(i).gameObject.SetActive(false);
         }
-
+        drone.droneRotorAudio.enabled= false;
     }
 
     public void onExit(DroneController drone)
