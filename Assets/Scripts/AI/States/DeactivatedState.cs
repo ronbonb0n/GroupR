211462e35c375fc6bcc081eb8d54ba8d0e335129
James <74412992+James-Joslin@users.noreply.github.com>
2023-01-03 15:34:25 +0000

    Drone audio

--- Assets/Scripts/AI/States/DeactivatedState.cs
+++ Assets/Scripts/AI/States/DeactivatedState.cs
@@ -14,6 +14,7 @@ public class DeactivatedState : IDroneState
         drone.SetScannerColor(Color.red);
         drone.navMeshAgent.enabled = false;
         drone.skinnedMesh.AddComponent<Rigidbody>();
+        drone.rotorAudioPlayer.enabled = false;
 
         // Deactivate scanners and text
         for(int i=0; i<drone.body.transform.childCount; i++)
