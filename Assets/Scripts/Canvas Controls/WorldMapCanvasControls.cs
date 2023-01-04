d4c8f3882ad78c77c09022b53d3e8dc1370d4499
ronbonb0n <42894074+ronbonb0n@users.noreply.github.com>
2023-01-04 16:13:12 +0000

    Adding narrative to world map start

--- Assets/Scripts/Canvas Controls/WorldMapCanvasControls.cs
+++ Assets/Scripts/Canvas Controls/WorldMapCanvasControls.cs
@@ -1,21 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class WorldMapCanvasControls : MonoBehaviour
 {
     public GameObject ControlsBtn;
     public GameObject ClearScreen;
+    public GameObject NarrationScreen;
+    public TextMeshProUGUI narrativeText;
+    public NarrativeScript narrativeScript;
+    private int dialogueNumber = 0;
 
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
         ControlsBtn.SetActive(true);
-        ClearScreen.SetActive(false);   
+        ClearScreen.SetActive(false);
+        NarrationScreen.SetActive(false);
     }
+
     public void OnToggle()
     {
         ControlsBtn.SetActive(!ControlsBtn.activeInHierarchy);
         ClearScreen.SetActive(!ClearScreen.activeInHierarchy);
+    }
+    public void onNarrationScreen()
+    {
+        NarrationScreen.SetActive(true);
+        dialogueNumber = 0;
+        narrativeText.text = narrativeScript.getNextText(dialogueNumber);
+        narrativeScript.onNarrative();
+
+    }
+    public void offNarrationScreen()
+    {
+        GameManager.State = GAME_STATE.WORLD_MAP;
+        NarrationScreen.SetActive(false);
+        narrativeScript.offNarrative();
+    }
+    public void continueNarrative()
+    {
+        if (narrativeScript.getNextText(dialogueNumber + 1) == "-1")
+        {
+            offNarrationScreen();
+        }
+        else
+        {
+            narrativeText.text = narrativeScript.getNextText(dialogueNumber + 1);
+            dialogueNumber++;
+        }
     }
 }
